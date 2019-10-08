43099c5ce655fe5716ca972b28329b2272b4806a
Jérémy Somsouk <jh.somsouk@gmail.com>
2019-10-08 23:10:27 +0200

    Twitch - fix 1st ex

---
+++ Solutions/Uber/src/Uber/Problem1.java
@@ -0,0 +1,31 @@
+package Uber;
+
+/*
+
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all
+the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24].
+If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?
+
+*/
+
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+class Problem1 {
+
+    private static ScheduledExecutorService executor;
+
+    public static void main(String args[]) {
+    }
+
+    private static List<Integer> scheduleTask(Runnable runnable, int delay) {
+
+    }
+}
