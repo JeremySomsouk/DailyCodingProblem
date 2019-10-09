79de117728cd124731c438227c51b62f760326fc
Jérémy Somsouk <jh.somsouk@gmail.com>
2019-10-09 21:36:23 +0200

    Snapchat - 1st ex

---
+++ Solutions/Snapchat/src/Snapchat/Problem1.java
@@ -0,0 +1,58 @@
+package Snapchat;
+
+/*
+
+Given an array of time intervals (start, end) for classroom lectures (possibly overlapping),
+find the minimum number of rooms required.
+
+For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
+
+*/
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class Problem1 {
+
+    static int overlappingRooms(List<Pair> inputList) {
+
+        List<Pair> startListWeighted = inputList.stream()
+                .map(interval -> new Pair(interval.getX(), 1))
+                .collect(Collectors.toList());
+
+        List<Pair> endListWeighted = inputList.stream()
+                .map(interval -> new Pair(interval.getY(), -1))
+                .collect(Collectors.toList());
+
+        List<Integer> weightsInOrder = Stream.concat(startListWeighted.stream(), endListWeighted.stream())
+                .sorted(Comparator.comparingInt(Pair::getX))
+                .map(Pair::getY)
+                .collect(Collectors.toList());
+
+        int maxRoomsNeeded = 0;
+        int currentOccupation = 0;
+        for (Integer weight : weightsInOrder) {
+            currentOccupation += weight;
+            if (maxRoomsNeeded < currentOccupation) {
+                maxRoomsNeeded = currentOccupation;
+            }
+        }
+
+        return maxRoomsNeeded;
+    }
+
+
+    @Getter
+    @AllArgsConstructor
+    @EqualsAndHashCode
+    static class Pair {
+        private int x;
+        private int y;
+    }
+}
---
+++ Solutions/Snapchat/test/Snapchat/Test1.java
@@ -0,0 +1,65 @@
+package Snapchat;
+
+/*
+
+Given an array of time intervals (start, end) for classroom lectures (possibly overlapping),
+find the minimum number of rooms required.
+
+For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
+
+*/
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static Snapchat.Problem1.Pair;
+import static Snapchat.Problem1.overlappingRooms;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Test1 {
+
+    @Test
+    void overlappingRooms_shouldReturnOne_whenOnInterval() {
+
+        List<Pair> input = Arrays.asList(
+                new Pair(60, 150)
+        );
+
+        assertThat(overlappingRooms(input)).isEqualTo(1);
+    }
+    @Test
+    void overlappingRooms_shouldReturnTwo_whenTwoOverlappingIntervals() {
+
+        List<Pair> input = Arrays.asList(
+                new Pair(30, 75),
+                new Pair(0, 50),
+                new Pair(60, 150)
+        );
+
+        assertThat(overlappingRooms(input)).isEqualTo(2);
+    }
+
+    @Test
+    void overlappingRooms_shouldReturnThree_whenThreeOverlappingIntervals() {
+
+        List<Pair> input = Arrays.asList(
+                new Pair(30, 75),
+                new Pair(0, 50),
+                new Pair(65, 200),
+                new Pair(60, 150)
+        );
+
+        assertThat(overlappingRooms(input)).isEqualTo(3);
+    }
+
+    @Test
+    void overlappingRooms_shouldReturnEmpty_whenEmptyInput() {
+
+        List<Pair> input = Collections.emptyList();
+
+        assertThat(overlappingRooms(input)).isEqualTo(0);
+    }
+}
